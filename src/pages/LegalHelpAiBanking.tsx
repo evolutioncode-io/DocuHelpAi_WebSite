e0864725ee9226ee8b96281790fa1af7e3e0941c
axelmonroy-evolution-code <axel.monroy@evolutioncode.io>
2026-03-24 11:24:41 -0600

    video banking changed

--- src/pages/LegalHelpAiBanking.tsx
+++ src/pages/LegalHelpAiBanking.tsx
@@ -268,7 +268,7 @@ function LegalHelpAiBanking() {
                         <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
                             <iframe
                                 className="absolute top-0 left-0 w-full h-full rounded-2xl shadow-2xl"
-                                src="https://www.youtube.com/embed/EqYHvbZACN8"
+                                src="https://www.youtube.com/embed/mGWLNkYF99c"
                                 title="LegalHelpAI Demo"
                                 frameBorder="0"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
